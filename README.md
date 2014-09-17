ee216138333f1e06eab3f8c6bc44258582d849ce
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2014-09-17 15:27:37 +0200

    Everything to build Open Web Apps for the Firefox platform.

---
+++ README.md
@@ -0,0 +1,10 @@
+# CanYouFillIt
+CanYouFillIt is a Breakout-style game mixed with snooker. It is freely inspired by the game available at [gimmefrictionbaby.com](http://gimmefrictionbaby.com). It has been developed with HTML5 technologies, so it should work on any modern browser (including mobile ones).
+
+## License
+This game is released under the terms of the MIT License. See the LICENSE file for more details.
+
+Some parts of the game use:
+
+- The Bootstrap framework, which is licensed under MIT (see [license](https://github.com/twbs/bootstrap/blob/master/LICENSE)).
+- The stats.js, which is licensed under MIT (see [license](https://github.com/mrdoob/stats.js/blob/master/LICENSE)).
--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ "$#" -ne "1" ]
+then
+	echo "You need to specity the url where the game will be published."
+	exit
+fi
+
+url=$( ruby -ruri -e "print '$1'.to_s.chomp('/')" )
+origin=$( ruby -ruri -e "print URI.join('$1', '/').to_s.chomp('/')" )
+path=$( ruby -ruri -e "print URI.parse('$1').path.chomp('/')" )
 
 [ -d dist ] || mkdir dist
 
-cp index.html requestAnimationFrame.min.js stats.min.js observable.js canyoufillit.js canyoufillit_canvas_gui.js .htaccess dist/
-
+## Build the offline app
+cp requestAnimationFrame.min.js stats.min.js observable.js canyoufillit.js canyoufillit_canvas_gui.js .htaccess dist/
+cp index.html icon-128.png icon-512.png bootstrap.css dist/
 sed 's/<html>/<html manifest="cache.manifest">/g' play.html > dist/play.html
 
 # Compute a hash of all the files that need to be cached.
@@ -12,4 +23,24 @@ do
 	echo `sha1sum $f`
 done|sha1sum|cut -d ' ' -f1)
 
+# and put it in the cache manifest, in order to make it unique.
 sed "s/# hash xyz/# hash $h/g" cache.manifest > dist/cache.manifest
+
+## For testing purpose, keep an online version of the game.
+cp play.html dist/play_online.html
+
+## Build the hosted open web app manifest
+sed "s!PATH!$path!g" manifest.webapp > dist/manifest.webapp
+
+## Build the packaged open web app
+[ -d tmp ] || mkdir tmp
+
+cp play.html requestAnimationFrame.min.js stats.min.js observable.js canyoufillit.js canyoufillit_canvas_gui.js tmp/
+cp icon-128.png icon-512.png tmp/
+sed "/appcache_path/d" manifest.webapp|sed "s!PATH!!g" > tmp/manifest.webapp
+
+zip dist/CanYouFillIt-WebApp.zip -j -r tmp
+
+sed "s!URL!$url!g" package.webapp > dist/package.webapp
+
+rm -rf tmp
