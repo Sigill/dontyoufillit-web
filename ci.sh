b161a577add30b3d613b6f78d310b0ab3710ed51
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2025-09-14 16:41:07 +0200

    Add ci.sh script to build in a container with a clean git repo

---
+++ ci.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+docker volume create dontyoufillit-ci-cache
+docker build -t dontyoufillit-ci -f docker/Dockerfile docker/
+
+docker run --rm -v $PWD:/repo:ro --mount source=dontyoufillit-ci-cache,target=/cache dontyoufillit-ci bash -c "
+set -e
+git config --global --add safe.directory /repo/.git
+git clone --depth 1 -b ${current_branch} /repo /src
+cd /src
+mkdir -p /cache/node_modules
+rsync -a /cache/node_modules/ /src/node_modules/
+npm i --prefer-offline --no-audit --progress=false
+rsync -a /src/node_modules/ /cache/node_modules/
+npm run build
+tree www/
+" && echo SUCCESS || echo FAILURE
