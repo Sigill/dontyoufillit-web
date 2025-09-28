5deba6301bfcf88a3bde572516e1745695db360e
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2025-09-28 11:58:48 +0200

    Use eslint defineConfig() helper instead of typescript-eslint config()

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
